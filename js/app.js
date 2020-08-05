c0c73388898ce365dcda3c00f71ceaa2fdacdce8
Pearcee <steven@gmail.com>
2020-08-05 16:53:46 +0100

    20:08:05:16:53:46

--- js/app.js
+++ js/app.js
@@ -8,3 +8,18 @@ if ("serviceWorker" in navigator) {
       .catch(err => console.log("service worker not registered", err));
   });
 }
+
+var x = document.getElementById("demo");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;
+}
